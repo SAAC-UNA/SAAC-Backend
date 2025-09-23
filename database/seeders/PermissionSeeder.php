f26b3ecfc237796838d18998e230c2eed2141d3f
Naydelin Jirón Castellón <nayidelin.jiron.castellon@est.una.ac.cr>
2025-09-23 08:53:24 -0600

    guarde cambios que no se subieron al parecer del seeder ya que los permisos no se cambiaron ah config/permissions.php

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -1,32 +1,20 @@
 <?php
-
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
-
 
 class PermissionSeeder extends Seeder
 {
     public function run(): void
     {
-        $modulos = [
-            'Usuarios'   => ['gestion_usuarios'], // duda si poner gestion_usuarios directo sin bloques
-            'Evidencias' => ['gestion_evidencias'],
-            'Reportes'   => ['gestion_reportes'],
-            'Cyclos'     => ['gestion_ciclos'],
-            'Programas'  => ['gestion_programas'],
-            'roles'      => ['gestion_roles'],
-        ];
+        $permisos = config('permissions.list'); // lee desde config/permissions.php
 
-         foreach ($modulos as $modulo => $listaPermisos) {
-            foreach ($listaPermisos as $permiso) {
-                Permission::firstOrCreate([
-                    'name'       => $permiso,
-                    'guard_name' => 'api',
-                ]);
-            }
+        foreach ($permisos as $permiso) {
+            Permission::firstOrCreate([
+                'name'       => $permiso,
+                'guard_name' => 'api',
+            ]);
         }
     }
 }
