d761b22b69f79f57cf16d1755c6fffe0dd183556
Naydelin Jirón Castellón <nayidelin.jiron.castellon@est.una.ac.cr>
2025-10-08 08:33:12 -0600

    Avances de HU004 - Acceso de esqueleto de Seders de ciclos y proceso

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PermissionSeeder::class,
+             #UserRoleCareerSeeder::class,
+              #ProcessCycleSeeder::class,
             
             
         ]);
