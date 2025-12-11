e24f0f4264ed5e31f1fc3146824898d85ab1ece7
José Jara Arias <jose.jara.arias@est.una.ac.cr>
2025-12-11 14:24:24 -0600

    arreglado error de sintaxis en app.php de bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -30,6 +30,8 @@ return Application::configure(basePath: dirname(__DIR__))
         // Habilitar CORS para desarrollo
         $middleware->validateCsrfTokens(except: [
             'api/*',
+        ]);
+
         // Middleware para renovar sesión en Redis en cada petición autenticada
         $middleware->alias([
             'refresh.session' => \App\Http\Middleware\RefreshSessionMiddleware::class,
